550c2a0d2a988ec47ec9ce5abb99cfde798beddd
Yeison <enmanuelquiterios@gmail.com>
2021-11-26 23:44:30 -0500

    Added director data to the movie serializer using AMS

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
+  # Adding this belongs_to will add the json data from the director controller to the movie return Please check the routes
+  belongs_to :director
   
 end
